fd2f1fa0e7e83b046c39b25f6b88d41f146d8600
Andy Shen <andy@shenie.info>
2010-12-04 11:03:33 +1100

    ssh completion using .ssh/config

--- completion/ssh.completion.bash
+++ completion/ssh.completion.bash
@@ -4,6 +4,7 @@
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
 _sshcomplete() {
+<<<<<<< HEAD
     
     # parse all defined hosts from .ssh/config
     if [ -r $HOME/.ssh/config ]; then
@@ -18,6 +19,12 @@ _sshcomplete() {
     fi
     
     return 0
+=======
+    if [ -f $HOME/.ssh/config ]; then
+        COMPREPLY=($(compgen -W "`ruby -e"puts open('${HOME}/.ssh/config', 'r') { |f| f.readlines }.find_all { |l| l =~ /^Host/ }.inject([]) { |hosts, line| hosts << line[5..-1].split }.flatten.sort.uniq"`" -- ${COMP_WORDS[COMP_CWORD]}))
+        return 0
+    fi
+>>>>>>> ssh completion using .ssh/config
 }
 
 complete -o default -o nospace -F _sshcomplete ssh
