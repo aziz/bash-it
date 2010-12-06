eca4b518940b7d60d152590b53dea3cbe4376655
Florian Baumann <flo@noqqe.de>
2010-12-06 16:01:06 +0100

    added advanced ssh completion

--- completion/ssh.completion.bash
+++ completion/ssh.completion.bash
@@ -5,10 +5,14 @@ export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
 _sshcomplete() {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> added advanced ssh completion
     
     # parse all defined hosts from .ssh/config
     if [ -r $HOME/.ssh/config ]; then
         COMPREPLY=($(compgen -W "$(grep ^Host $HOME/.ssh/config | awk '{print $2}' )" -- ${COMP_WORDS[COMP_CWORD]}))
+<<<<<<< HEAD
     fi
 
     # parse all hosts found in .ssh/known_hosts
@@ -25,6 +29,18 @@ _sshcomplete() {
         return 0
     fi
 >>>>>>> ssh completion using .ssh/config
+=======
+    fi
+
+    # parse all hosts found in .ssh/known_hosts
+    if [ -r $HOME/.ssh/known_hosts ]; then
+        if grep -v -q -e '^ ssh-rsa' $HOME/.ssh/known_hosts ; then
+        COMPREPLY=( $COMPREPLY $(compgen -W "$( awk '{print $1}' $HOME/.ssh/known_hosts | cut -d, -f 1 | sed -e 's/\[//g' | sed -e 's/\]//g' | cut -d: -f1 | grep -v ssh-rsa)" -- ${COMP_WORDS[COMP_CWORD]} )) 
+        fi
+    fi
+    
+    return 0
+>>>>>>> added advanced ssh completion
 }
 
 complete -o default -o nospace -F _sshcomplete ssh
