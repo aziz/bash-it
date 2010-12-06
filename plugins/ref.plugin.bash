5394ac54b2f5b32f39b27b9fee40d29f83fb25d7
Allen Bargi <allen.bargi@gmail.com>
2010-12-06 18:36:04 +0100

    change chmod for colors.bash and comments cleanup for ref plugin

--- plugins/ref.plugin.bash
+++ plugins/ref.plugin.bash
@@ -1,4 +1,116 @@
 #####################################################################################################################################################################
+<<<<<<< HEAD
+# README
+# ------
+#
+# ref is a plugin for storing HTML formatted references, mainly suited for programming.
+# Your $REF_DIR variable is the directory for storing these references in. If it does not exist, it will be created automatically.
+# Here is an example of what my $REF_DIR looks like, because this will be of use when I explain how your $REF_DIR has to be structured:
+#
+# ~/.ref/
+#   ruby/
+#     general/
+#       index.html
+#   bash/
+#     array/
+#       index.html
+#     select/
+#       index.html
+#
+# This is what the basic structure of your $REF_DIR should look like: Subdirectories for each subject, and then another set of subdirectories for the part of the
+# subject you want to reference. And in the second subdirectory, an index.html file.
+#
+# To use ref, you do the ref command followed by the subject and the sub-subject as arguments. For instance:
+#
+=======
+<<<<<<< HEAD
+# README 																																							
+# ------ 																																							
+# 																																									
+# ref is a plugin for storing HTML formatted references, mainly suited for programming. 																			
+# Your $REF_DIR variable is the directory for storing these references in. If it does not exist, it will be created automatically. 									
+# Here is an example of what my $REF_DIR looks like, because this will be of use when I explain how your $REF_DIR has to be structured: 							
+# 																																									
+# ~/.ref/ 																																							
+#   ruby/ 																																							
+#       general/ 																																					
+#  			index.html 																																				
+# 	bash/ 																																							
+# 		array/ 																																						
+# 			index.html 																																				
+# 		select/ 																																					
+# 			index.html 																																				
+# 																																									
+# This is what the basic structure of your $REF_DIR should look like: Subdirectories for each subject, and then another set of subdirectories for the part of the   
+# subject you want to reference. And in the second subdirectory, an index.html file. 																				
+# 
+# To use ref, you do the ref command followed by the sugject and the sub-subject as arguments. For instance:
+# 
+>>>>>>> change chmod for colors.bash and comments cleanup for ref plugin
+# ref bash array
+#
+# Would open the bash/array/index.html file.
+#
+# To list your references, you would do the ref ls command, optionally followed by a subject. For instance:
+#
+# ref ls
+#
+# Would give me:
+#
+# ruby bash
+#
+# And:
+#
+# ref ls bash
+#
+# would output:
+#
+# array
+# select
+#
+# I hope that you like this plugin and if you have any questions about it, send me (mrman208) a message on GitHub or email me at mrman208@me.com
+#####################################################################################################################################################################
+
+ref() {
+  if [ ! -d "$REF_DIR" ]
+  then
+    mkdir -p "$REF_DIR"
+  fi
+
+  REF_DIR=${REF_DIR%/}
+
+  if [ "$1" = 'ls' ]
+  then
+    if [ "$2" = '' ]
+    then
+      builtin cd "$REF_DIR"
+      ls -G
+      builtin cd - > /dev/null
+      return
+    else
+      builtin cd "$REF_DIR"/"$2"
+      ls -G
+      builtin cd - > /dev/null
+      return
+    fi
+  fi
+
+  DIR="${1}/${2}"
+
+  builtin cd "$REF_DIR"/"$DIR"
+
+<<<<<<< HEAD
+=======
+	builtin cd "$REF_DIR"/"$DIR"
+
+	if [ $(uname) = "Darwin" ]
+	then
+		open index.html
+	elif [ $(uname) = "Linux" ]
+	then
+		gnome-open index.html
+	fi
+=======
 # README
 # ------
 #
@@ -73,6 +185,7 @@ ref() {
 
   builtin cd "$REF_DIR"/"$DIR"
 
+>>>>>>> change chmod for colors.bash and comments cleanup for ref plugin
   if [ $(uname) = "Darwin" ]
   then
     open index.html
@@ -80,4 +193,8 @@ ref() {
   then
     gnome-open index.html
   fi
+<<<<<<< HEAD
+=======
+>>>>>>> change chmod for colors.bash and comments cleanup for ref plugin
+>>>>>>> change chmod for colors.bash and comments cleanup for ref plugin
 }
