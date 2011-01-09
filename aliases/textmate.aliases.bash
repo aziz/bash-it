eec95a3d7a1538aa0a6e505b23207c193f7e64ab
Allen Bargi <allen.bargi@gmail.com>
2011-01-09 15:42:08 +0100

    removed not working stuff

--- aliases/textmate.aliases.bash
+++ aliases/textmate.aliases.bash
@@ -8,5 +8,4 @@ if [[ -x `which mate` ]]; then
 fi
 
 alias e='mate'
-alias et='mate app config db lib public script test spec config.ru Gemfile Rakefile README &'
-
+alias et='mate app config db doc lib public test spec vendor Gemfile Rakefile TODO .gitignore .rvmrc &'
