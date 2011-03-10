ca43536057f819e945f9c7c4c99c53cf9796faa2
Allen Bargi <allen.bargi@gmail.com>
2011-03-10 20:48:26 +0100

    bold red color for git dirty

--- themes/aziz/aziz.theme.bash
+++ themes/aziz/aziz.theme.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCM_THEME_PROMPT_DIRTY=" ${red}✖"
+SCM_THEME_PROMPT_DIRTY=" ${bold_red}✖"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✔"
 SCM_THEME_PROMPT_PREFIX=" "
 SCM_THEME_PROMPT_SUFFIX=" "
@@ -7,7 +7,7 @@ SCM_THEME_PROMPT_SUFFIX=" "
 PROMPT="\[${bold_yellow}\]\w\[${bold_blue}\] \[\$(scm_char)\]\[\$(scm_prompt_info)\]\[${normal}\]\[${reset_color}\]→ "
 
 # git theming
-GIT_THEME_PROMPT_DIRTY=" ${red}✖"
+GIT_THEME_PROMPT_DIRTY=" ${bold_red}✖"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✔"
 GIT_THEME_PROMPT_PREFIX=""
 GIT_THEME_PROMPT_SUFFIX=""
