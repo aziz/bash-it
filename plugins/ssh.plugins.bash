26a9bd551a931cbac2e2b5f8e99e7388a2ca0edb
Allen Bargi <allen.bargi@gmail.com>
2011-03-28 21:31:03 +0200

    better naming for ssh plugin functions

--- plugins/ssh.plugins.bash
+++ plugins/ssh.plugins.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-function add_ssh() {
+function ssh_add() {
   echo -en "\n\nHost $1\n  HostName $2\n  User $3\n  ServerAliveInterval 30\n  ServerAliveCountMax 120" >> ~/.ssh/config
 }
 
-function sshlist() {
+function ssh_list() {
   awk '$1 ~ /Host$/ { print $2 }' ~/.ssh/config
 }
