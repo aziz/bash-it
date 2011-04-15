30009e2183cc62a1fcf70f27a789213220f49fab
Allen Bargi <allen.bargi@gmail.com>
2011-04-15 10:27:29 +0200

    added bundler aliases

--- aliases/rails.aliases.bash
+++ aliases/rails.aliases.bash
@@ -31,6 +31,13 @@ alias rr='rake routes'
 alias rdbm='rake db:migrate'
 alias rdbr='rake db:rollback'
 
+# bundler
+alias b="bundle"
+alias bi="b install --path vendor"
+alias bu="b update"
+alias be="b exec"
+alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
+
 
 function rails-help() {
   echo "Rails Aliases Usage"
